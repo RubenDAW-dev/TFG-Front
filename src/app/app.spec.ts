76e36d70b13ced229c8f5e94f11c6ded6e64f908
AzureAD\RubenVillarGonzalez <rvillar@hiberus.com>
2026-03-24 10:01:00 +0100

    Test del front

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideRouter } from '@angular/router';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [App],
-    }).compileComponents();
+      providers: [
+        provideRouter([])
+      ]
+    });
   });
 
   it('should create the app', () => {
@@ -14,10 +19,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
+    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TFG');
+    expect(compiled.querySelector('h1')?.textContent).toContain('TFG');
   });
 });
