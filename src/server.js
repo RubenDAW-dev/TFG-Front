5c8afca43bdd8c9771ca4c23d3eafaf5c42116d8
Ruben <daw15.jesuitas.2025@gmail.com>
2026-04-27 20:12:54 +0200

    cambios del server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs'); 
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const distFolder = fs.readdirSync(path.join(__dirname, 'dist'))[0];
-const distPath = path.join(__dirname, 'dist', distFolder, 'browser');
+// Subir un nivel desde src/ al wwwroot/
+const baseDir = path.join(__dirname, '..');
+const distFolder = fs.readdirSync(path.join(baseDir, 'dist'))[0];
+const distPath = path.join(baseDir, 'dist', distFolder, 'browser');
 
+console.log('Base directory:', baseDir);
 console.log('Using dist path:', distPath);
 
 app.use(express.static(distPath));
